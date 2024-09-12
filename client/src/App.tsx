b92fe3a270851b0c5ffad852f80f2be0b7d7c515
AndreiTolstoi <tolstoi.a@gmail.com>
2024-09-12 19:14:07 +0300

    add(client): routes

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import { io } from 'socket.io-client';
+import Home from './components/home/Home';
+import ChatPage from './components/chat/ChatPage';
+
+const socket = io('http://localhost:5000');
+
+function App() {
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/chat" element={<ChatPage />} />
+			</Routes>
+		</>
+	);
+}
+
+export default App;
